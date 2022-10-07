ebb349725dc28225263a9c6f5327c3fadce43459
jrahemtulla <96964093+jrahemtulla@users.noreply.github.com>
2022-10-07 17:39:19 -0400

    Add files via upload

---
+++ books_business.sql
@@ -0,0 +1,93 @@
+DROP DATABASE IF EXISTS `books`;
+CREATE DATABASE IF NOT EXISTS `books`;
+USE `books`;
+
+-- TABLE Books -- 
+CREATE TABLE `Authors` (
+	`AuthorID` int NOT NULL AUTO_INCREMENT,
+    `AuthorName` varchar (50) NOT NULL,
+    PRIMARY KEY (`AuthorID`),
+    INDEX `AuthorID` (`AuthorID` ASC),
+    INDEX `AuthorName` (`AuthorName` ASC)
+    ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
+    
+CREATE TABLE `Publishers` (
+	`PublisherID` int NOT NULL AUTO_INCREMENT,
+    `PublisherName` varchar (50) NOT NULL,
+    PRIMARY KEY (`PublisherID`),
+    INDEX `PublisherID` (`PublisherID` ASC),
+    INDEX `PublisherName` (`PublisherName` ASC)
+    ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
+    
+    CREATE TABLE `Editors` (
+	`EditorID` int NOT NULL AUTO_INCREMENT,
+    `EditorName` varchar (50) NOT NULL,
+    PRIMARY KEY (`EditorID`),
+    INDEX `EditorID` (`EditorID` ASC),
+    INDEX `EditorName` (`EditorName` ASC)
+    ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
+    
+
+CREATE TABLE `Books` (
+	`BookID` 		int NOT NULL AUTO_INCREMENT,
+    `BookName` 		varchar (60) NOT NULL ,
+    `AuthorID` 		int NOT NULL,
+	`PublisherID` 		int NOT NULL,	
+    `EditorID` 		int NOT NULL,
+	`Genre` 		varchar (15) NULL ,
+	`Price` 		int NOT NULL ,
+    `Royalty` 		decimal(13,2),
+  	PRIMARY KEY (`BookID`),	
+	INDEX `BookID` (`BookID` ASC),
+	INDEX `BookName` (`BookName` ASC),	
+	FOREIGN KEY (`PublisherID`) REFERENCES `Publishers` (`PublisherID`),
+    FOREIGN KEY (`EditorID`) REFERENCES `Editors` (`EditorID`)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `Customers` (
+	`CustomerID` int NOT NULL AUTO_INCREMENT,
+    `CustomerName` varchar (50) NOT NULL,
+    PRIMARY KEY (`CustomerID`),
+    INDEX `CustomerID` (`CustomerID` ASC),
+    INDEX `CustomerName` (`CustomerName` ASC)
+    ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `Orders` (
+	`OrderID` int NOT NULL AUTO_INCREMENT,
+    `Amount` int NOT NULL ,
+    PRIMARY KEY (`OrderID`),
+    INDEX `OrderID` (`OrderID` ASC),
+    INDEX `Amount` (`Amount` ASC)
+    ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `OrderDetails` (
+`OrderID` int NOT NULL,
+`BookID` int NOT NULL, 
+`Price` int NOT NULL,
+`CustomerID` int NOT NULL,
+PRIMARY KEY (`OrderID`, `BookID`,`CustomerID`),
+FOREIGN KEY (`OrderID`) REFERENCES `Orders` (`OrderID`)
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION,
+FOREIGN KEY (`BookID`) REFERENCES `Books` (`BookID`)
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION,
+FOREIGN KEY (`CustomerID`) REFERENCES `Customers` (`CustomerID`)
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `BookAuthors` (
+`BookID` int NOT NULL,
+`AuthorID` int NOT NULL, 
+PRIMARY KEY (`BookID`, `AuthorID`),
+FOREIGN KEY (`BookID`) REFERENCES `Books` (`BookID`)
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION,
+FOREIGN KEY (`AuthorID`) REFERENCES `Authors` (`AuthorID`)
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
